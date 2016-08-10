9d4d47e203d686db9aaaa036a4a763365bfa3752
Bob <kosten_37@mail.ru>
2016-08-10 16:32:24 +0600

    Set worked test proj

--- TestMarionette/TestMarionette/js/app/CusApp.Router.js
+++ TestMarionette/TestMarionette/js/app/CusApp.Router.js
@@ -3,7 +3,7 @@
 (function () {
     CusApp.Router = Backbone.Marionette.AppRouter.extend({
         appRoutes: {
-            '*filter': 'filterItems'
+            'qwe': 'ss'
         }
     });
 
@@ -13,31 +13,12 @@
         },
         start: function () {
             this.showRootLayout(this.cusList);
-            this.showFooter(this.cusList);
-            this.showcusList(this.cusList);
-            this.cusList.on('all', this.updateHiddenElements, this);
-            this.cusList.fetch();
         },
-        updateHiddenElements: function () {
-            $('#main, #footer').toggle(!!this.cusList.length);
-        },
-
         showRootLayout: function (cusList) {
-            var header = new TodoMVC.HeaderLayout({
+            var header = new CusApp.RootLayout({
                 collection: cusList
             });
-            TodoMVC.App.root.showChildView('header', header);
-        },
-        showcusList: function (cusList) {
-            TodoMVC.App.root.showChildView('main', new TodoMVC.ListView({
-                collection: cusList
-            }));
-        },
-
-        // Set the filter to show complete or all items
-        filterItems: function (filter) {
-            var newFilter = filter && filter.trim() || 'all';
-            filterChannel.request('filterState').set('filter', newFilter);
+            CusApp.App.root.showChildView('header', header);
         }
     });
 })();
