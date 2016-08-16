21d59302805148003a6a2420d6158f640ff657b6
Bob <kosten_37@mail.ru>
2016-08-16 12:07:14 +0700

    Try inderstands require

--- TestMarionette/TestMarionette/js/app/CusApp.App.js
+++ TestMarionette/TestMarionette/js/app/CusApp.App.js
@@ -1,16 +1,20 @@
 var CusApp = CusApp || {};
 
-(function () {
-    var App = Backbone.Marionette.Application.extend({
-        setRootLayout: function () {
-            this.root = new CusApp.RootLayout();
-        }
-    });
 
-    CusApp.App = new App();
+window.App.Define('../../js/app/CusApp.App.js', [], function (pExport) {
+    (function () {
+        var App = Backbone.Marionette.Application.extend({
+            setRootLayout: function () {
+                //                this.root = new CusApp.RootLayout();
+                console.log("I`m started ololo");
+            }
+        });
 
-    CusApp.App.on('before:start', function () {
-        CusApp.App.setRootLayout();
-    });
+        CusApp.App = new App();
 
-})();
+        CusApp.App.on('before:start', function () {
+            CusApp.App.setRootLayout();
+        });
+
+    })();
+});
--- TestMarionette/TestMarionette/js/app/CusApp.js
+++ TestMarionette/TestMarionette/js/app/CusApp.js
@@ -1,15 +1,19 @@
 var CusApp = CusApp || {};
+App.debug = 1;
 
-$(function () {
-    CusApp.App.on('start', function () {
-        var controller = new CusApp.Controller();
-        controller.router = new CusApp.Router({
-            controller: controller
-        });
+window.App.Require([
+    '../../js/app/CusApp.App.js'
+],
+function () {
+//    CusApp.App.on('start', function () {
+//        var controller = new CusApp.Controller();
+//        controller.router = new CusApp.Router({
+//            controller: controller
+//        });
 
-        controller.start();
-        Backbone.history.start();
-    });
+//        controller.start();
+//        Backbone.history.start();
+//    });
 
     CusApp.App.start();
 });
